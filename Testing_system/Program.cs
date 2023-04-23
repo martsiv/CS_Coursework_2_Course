da8dc41c52d8c2be6638c2e945184da489cfe186
martsiv <mykola.martsiv@gmail.com>
2023-04-23 21:50:32 +0300

    Add project files.

---
+++ Testing_system/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Testing_system
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
